71753de46642f5ae1594691cef875c67a10f12e8
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-03-25 13:15:09 +0100

    fix errorInfo bug

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import PackageCards from "../components/PackageCards";
 import "../styles/Track.css";
@@ -6,18 +7,19 @@ import "../styles/Track.css";
 function Track() {
   const url = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
   const [packages, setPackages] = useState([]);
+  const [errorInfo, setErrorInfo] = useState([]);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        setPackages(json);
-      });
+    axios
+      .get(url)
+      .then((res) => setPackages(res.data))
+      .catch((error) => setErrorInfo(error.message));
   }, []);
 
   return (
     <div className="package-list">
       <PackageCards packages={packages} />
+      {errorInfo && <h1>Something happened! We will fix it asap!</h1>}
     </div>
   );
 }
