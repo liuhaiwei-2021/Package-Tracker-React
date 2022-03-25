8468bc38f5a848237813f1fef9f63776f3dd10cf
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-03-25 13:22:19 +0100

    error bug

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -7,7 +7,7 @@ import "../styles/Track.css";
 function Track() {
   const url = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
   const [packages, setPackages] = useState([]);
-  const [error, setError] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
