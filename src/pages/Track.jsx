97687ec8b68513e08fbb80e70174d997feb56a94
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-03-25 12:58:09 +0100

    change setErrorInfo name

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -7,7 +7,7 @@ import "../styles/Track.css";
 function Track() {
   const url = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
   const [packages, setPackages] = useState([]);
-  const [errorInfo, setError] = useState([]);
+  const [errorInfo, setErrorInfo] = useState([]);
 
   useEffect(() => {
     axios
