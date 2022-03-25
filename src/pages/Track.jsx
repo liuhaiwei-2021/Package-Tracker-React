4d5c859ccf1ec24fe74531633aa1088305a9965d
Haiwei Liu <liuhaiwei2016@gmail.com>
2022-03-25 12:50:32 +0100

    add axios instead of seperate async function

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import PackageCards from "../components/PackageCards";
 import "../styles/Track.css";
 
 function Track() {
-  const url = "https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
+  const url = "1https://my.api.mockaroo.com/insta-orders.json?key=e49e6840";
   const [packages, setPackages] = useState([]);
+  const [errorInfo, setError] = useState([]);
 
   useEffect(() => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        setPackages(json);
-      });
+    axios
+      .get(url)
+      .then((res) => setPackages(res.data))
+      .catch((error) => setError(error.message));
   }, []);
 
   return (
     <div className="package-list">
       <PackageCards packages={packages} />
+      {errorInfo && <h1>Something happened! We will fix it asap!</h1>}
     </div>
   );
 }
